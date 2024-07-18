02611e363967ee93a26a18fb5f9d5f442d042979
Dean <kuhtanyc@gmail.com>
2024-07-17 22:01:14 -0400

    refactored for 2024!

--- replace.pl
+++ replace.pl
@@ -20,18 +20,15 @@ Excel spreadsheet using OLE.
 sub excelFormat() {
 
   #output directory of the Excel spreadsheet
-  my $outputdir ='C:\Documents and
-Settings\kuhtad\Desktop\OLE\output.xls';
+  my $outputdir ='C:\Documents and Settings\kuhtad\Desktop\OLE\output.xls';
 
   #use existing instance if Excel is already running
   my $ex;
-  eval {$ex =
-Win32::OLE->GetActiveObject('Excel.Application')};
+  eval {$ex = Win32::OLE->GetActiveObject('Excel.Application')};
   die "Excel not installed" if $@;
        
   unless (defined $ex) {
-      $ex = Win32::OLE->new('Excel.Application', sub
-{$_[0]->Quit;})
+      $ex = Win32::OLE->new('Excel.Application', sub{$_[0]->Quit;})
       or die "cannot start Excel";
   }
 
@@ -61,23 +58,22 @@ Win32::OLE->GetActiveObject('Excel.Application')};
       my $name;my $ssn;my $dob;
       my $phone;my $misc;
    
-($name,$ssn,$dob,$phone,$misc)=split("\t",$line);
+      ($name,$ssn,$dob,$phone,$misc)=split("\t",$line);
  
       #write each row to the worksheet
       foreach($line){
 
-    #perform regexes on $name to eliminate specified
-strings;
-    #eliminate instances of "n/a","na","unknown","not
-known",
+      	#perform regexes on $name to eliminate specified strings;
+        #eliminate instances of "n/a","na","unknown","not known",
         $name =~ s/na//g;
         $name =~ s/n\/a//g;
         $name =~ s/unknown//g;
         $name =~ s/not known//g;
+
         #write filtered data to the appropriate cell
         $sheet->Cells($row,1)->{Value} = $name;
 
-    #eliminate any alphabetic strings and repeating
+    	#eliminate any alphabetic strings and repeating
         #numbers (ie: 11111,22222,33333,etc...)
         $ssn =~ s/[A-Za-z]/ /g;
         $ssn =~ s/[0-9]{10,}/ /g;
@@ -85,12 +81,9 @@ known",
         #write filtered data to the appropriate cell
         $sheet->Cells($row,2)->{Value} = $ssn;
 
-    #perform regexes on $dob to eliminate specified
-strings;
-    #eliminate instances of "n/a","na","unknown","not
-known",
-        #repeating numbers (ie:
-11111,22222,33333,etc...)
+    	#perform regexes on $dob to eliminate specified strings;
+    	#eliminate instances of "n/a","na","unknown","not known",
+        #repeating numbers (ie:11111,22222,33333,etc...)
         $dob =~ s/na//g;
         $dob =~ s/n\/a//g;
         $dob =~ s/unknown//g;
@@ -98,18 +91,15 @@ known",
         #write filtered data to the appropriate cell
         $sheet->Cells($row,3)->{Value} = $dob;
 
-    #eliminate any alphabetic strings and repeating
+    	#eliminate any alphabetic strings and repeating
         #numbers (ie: 11111,22222,33333,etc...)
         $phone =~ s/[A-Za-z]/ /g;
         #write filtered data to the appropriate cell
         $sheet->Cells($row,4)->{Value} = $phone;
 
-    #perform regexes on $misc to eliminate specified
-strings;
-    #eliminate instances of "n/a","na","unknown","not
-known",
-        #repeating numbers (ie:
-11111,22222,33333,etc...)
+    	#perform regexes on $misc to eliminate specified strings;
+    	#eliminate instances of "n/a","na","unknown","not known",
+        #repeating numbers (ie: 11111,22222,33333,etc...)
         $misc =~ s/na//g;
         $misc =~ s/n\/a//g;
         $misc =~ s/unknown//g;
