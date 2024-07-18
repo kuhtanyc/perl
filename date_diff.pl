328366069bb101e18ef6a39708459f8670028820
Dean <kuhtanyc@gmail.com>
2024-07-17 21:51:01 -0400

    Updated script to get it working in 2024!

--- date_diff.pl
+++ date_diff.pl
@@ -3,33 +3,38 @@
 # date_diff.pl
 #
 # Description: this program gets the difference
-# of two dates.
+# of two dates in days.
 #
 ################################################
 use Time::Local;
 
 $startdate = '07012005';
-$enddate  = '07042005';
+$enddate  = '07172024';
 
 $startsec = dateToSec($startdate);
 $endsec = dateToSec($enddate);
 $diff = ($startsec-$endsec)/(1000*3600*24)*-1000;
 
-print "$diff\n";
+print "difference: $diff days\n\n";
 
-if($diff>60){print "error: interval is greater than 60
-days\n";}
-else{print "60 or less\n";}
+if ($diff > 60) {
+	print "error: interval is greater than 60 days\n";
+}
+else {
+	print "60 or less\n";
+}
 
 sub dateToSec
 {
   $date = shift;
   $_ = $date;
   my($m, $d, $y) = /(\d{2})(\d{2})(\d{4})/;
-  $m--;                                    #months
-are 0-based
-  my $time = timegm(0, 0, 0, $d, $m, $y);  #GMT is
-DST-independent
+  $m--;                                    
+  #months are 0-based
+  
+  my $time = timegm(0, 0, 0, $d, $m, $y);  
+  #GMT is DST-independent
   #print scalar localtime($time);
+
   return $time;
 }
